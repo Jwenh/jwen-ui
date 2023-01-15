b4425eba395e90e55a568e700f7a784e643307ef
jwen <1132505874@qq.com>
2023-01-15 22:43:31 +0800

    tailwind css 配置

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'), //构建时提前处理 @import 语句
+    require('tailwindcss'),
+    require('autoprefixer'),  //浏览器前缀补全
+  ]
 }
